cb7053d34b0e9b196540c4c51e275cd1be48539c
kaiocesarnp <kaiocesarnp@gmail.com>
2022-09-25 17:26:05 -0300

    gerador de senha

---
+++ script.js
@@ -0,0 +1,35 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password") //pega o container input
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*()-_=+[]{}~^,.;<>:";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+   
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}//O 'i' começa em 0 (let i = 0) e roda o 'for' até que o segundo 'i' seja menor que o 
+  //sliderElement.value, que é o tanto de caracteres que o usuário selecionou, por exemplo
+  // 7 caracteres. A variavel 'n' pega os caracteres do 'let = charset';
+
+  function copyPassword(){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+  }
